385b646635422b5c4c4ded7978826fdd3753ad32
seokimun <gimun08@naver.com>
2025-12-14 16:08:57 +0900

    🔧 Delete author of the crud schema

---
+++ prisma/migrations/20251214040351_delete_author/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `authorName` on the `Crud` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Crud" DROP COLUMN "authorName";

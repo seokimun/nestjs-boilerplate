e38bf73b2410313bcba3a271e2446e8495e97b3c
seokimun <gimun08@naver.com>
2026-01-08 22:43:12 +0900

    feat: 🔧 API Version 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { V1Module } from './api/v1/v1.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [V1Module],
   controllers: [AppController],
   providers: [AppService],
 })

eea52f16e43ed25457b392ade600ab1bcdcb451f
seokimun <gimun08@naver.com>
2026-01-08 22:40:47 +0900

    feat: 🔧 Zod ValidationPipe 구축

---
+++ src/libs/pipe/zod-validation.pipe.ts
@@ -0,0 +1,19 @@
+import { BadRequestException, PipeTransform } from '@nestjs/common';
+import { prettifyError, ZodSchema } from 'zod';
+
+export class ZdoValidationPipe implements PipeTransform {
+  constructor(private schema: ZodSchema) {}
+
+  transform(value: unknown) {
+    const result = this.schema.safeParse(value);
+
+    if (!result.success) {
+      throw new BadRequestException({
+        message: 'Validation failed',
+        pretty: prettifyError(result.error), //운영 시 제거
+      });
+    }
+
+    return result.data;
+  }
+}

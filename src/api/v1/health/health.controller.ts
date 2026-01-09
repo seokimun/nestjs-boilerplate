9b3e5b4a78a4548bce539fbb066df2137370045d
seokimun <gimun08@naver.com>
2026-01-10 04:29:26 +0900

    feat: ✨ health check 기능 구현

---
+++ src/api/v1/health/health.controller.ts
@@ -0,0 +1,36 @@
+import { Controller, Get } from '@nestjs/common';
+import { HealthCheck, HealthCheckService } from '@nestjs/terminus';
+import { PrismaHealthIndicator } from '../../../libs/prisma/health/prisma.health';
+
+@Controller({ path: 'health', version: '1' })
+export class HealthController {
+  constructor(
+    private readonly health: HealthCheckService,
+    private readonly db: PrismaHealthIndicator,
+  ) {}
+
+  @Get()
+  @HealthCheck()
+  async healthCheck() {
+    const check = await this.health.check([
+      () => ({
+        app: { status: 'up' },
+      }),
+    ]);
+
+    return {
+      status: check.status,
+      details: check.details,
+    };
+  }
+
+  @Get('ready')
+  @HealthCheck()
+  async readyCheck() {
+    const check = await this.health.check([() => this.db.isHealth('db')]);
+    return {
+      status: check.status,
+      details: check.details,
+    };
+  }
+}
---
+++ src/libs/prisma/health/prisma.health.ts
@@ -0,0 +1,22 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { HealthIndicator, HealthIndicatorResult } from '@nestjs/terminus';
+import { PrismaService } from '../prisma.service';
+
+@Injectable()
+export class PrismaHealthIndicator extends HealthIndicator {
+  private logger = new Logger(PrismaHealthIndicator.name);
+
+  constructor(private readonly prisma: PrismaService) {
+    super();
+  }
+
+  async isHealth(key: string): Promise<HealthIndicatorResult> {
+    try {
+      await this.prisma.$queryRaw`SELECT 1`;
+      return this.getStatus(key, true);
+    } catch (e) {
+      this.logger.error('DB health check failed');
+      return this.getStatus(key, false);
+    }
+  }
+}

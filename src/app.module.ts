5c5b94b8b54a9bea22b7285b94b89c33ebd3c5c2
seokimun <gimun08@naver.com>
2026-01-13 01:07:34 +0900

    feat: ✨ env 검증 로직추가 및 전역가드 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,39 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { RolesGuard } from './api/v1/auth/guard/roles.guard';
+import { JwtAuthGuard } from './api/v1/auth/jwt/jwt.guard';
 import { V1Module } from './api/v1/v1.module';
+import { envSchema } from './libs/config/env.schema';
 import { LoggerMiddleware } from './libs/middleware/logger.middleware';
-import { PrismaModule } from './libs/prisma/prisma.module';
 
 @Module({
-  imports: [V1Module, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validate: (env) => {
+        const parsed = envSchema.safeParse(env);
+
+        if (!parsed.success) {
+          console.error('ENV VALIDATION ERROR:', parsed.error.format()); //개발환경
+          throw new Error('Invalid environment variables');
+        }
+
+        return parsed.data;
+      },
+    }),
+    V1Module,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
